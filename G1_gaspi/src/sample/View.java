875f3614b9988d6f82da7286c9348f47a47f517d
FlorentRO <florent.robert@etu.unice.fr>
2019-02-21 22:48:27 +0100

    Rien

--- G1_gaspi/src/sample/View.java
+++ G1_gaspi/src/sample/View.java
@@ -5,7 +5,6 @@ public class View {
     public static final String XML_FILE_Insc = "view/Inscription.fxml";
     public static final String XML_FILE_Menu = "view/Menu.fxml";
     public static final String XML_FILE_Cond = "view/Conditions.fxml";
-    //public static final String CSS = "sample/ressources/calculator.css";
     public static final String LABEL_Insc = "Inscription";
     public static final int WIDTH = 550;
     public static final int HEIGHT = 500;
