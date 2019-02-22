012871aec0aa45174c2c3d717c869c9d8512e0a3
FlorentRO <florent.robert@etu.unice.fr>
2019-02-22 20:15:34 +0100

    Menu+AllContent(-Annonces)

--- G1_gaspi/src/sample/View.java
+++ G1_gaspi/src/sample/View.java
@@ -7,7 +7,11 @@ public class View {
     public static final String XML_FILE_Cond = "view/Conditions.fxml";
     public static final String LABEL_Insc = "Inscription";
     public static final String XML_FILE_Produits = "view/Mes_produits.fxml";
-
+    public static final String XML_FILE_Course= "view/Liste_course.fxml";
+    public static final String XML_FILE_Historique = "view/Historique.fxml";
+    public static final String XML_FILE_Annonces = "view/Annonces.fxml";
+    public static final String XML_FILE_Amis = "view/Mes_amis.fxml";
+    public static final String XML_FILE_Compte = "view/Mon_compte.fxml";
     public void initMes_Produits(){
 
     }
